6bf16383355372cb80f188501f27f09fec9081d2
Makai <micro314ck@gmail.com>
2024-02-14 17:08:21 +0200

    Valentines

--- components/Card.js
+++ components/Card.js
@@ -1,16 +1,11 @@
-
+import Song from 'components/Song'
 
 export default function Card() {
     return (
       <div className="card">
         <h1 className="message">Hey baby!<br />Happy Valentine’s Day<br />To us, bene ba Love :)</h1>
         <div className="heart"></div>
-        <div className="signature">
-          <audio controls autoPlay>
-            <source src="/data/nikukonde.mp3" type="audio/mpeg" />
-            <source src="./data/nikukonde.mp3" type="audio/mpeg" />
-          </audio>
-        </div>
+        <Song />
       </div>
     );
   }
---
+++ components/Song.js
@@ -0,0 +1,10 @@
+export default function Container() {
+    return (
+        <div className="signature">
+        <audio controls autoPlay>
+          <source src="/nikukonde.mp3" type="audio/mpeg" />
+          <source src="/nikukonde.mp3" type="audio/mpeg" />
+        </audio>
+      </div>
+    );
+  }
