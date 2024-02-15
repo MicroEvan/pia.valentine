d67b1b50a0b387fbf630195b61da95a2eda04b50
Makai <micro314ck@gmail.com>
2024-02-15 16:10:38 +0200

    New song

--- components/Song.js
+++ components/Song.js
@@ -3,8 +3,8 @@ export default function Song() {
   return (
     <div class="audio-container">
      <audio controls autoPlay>
-            <source src="/nikukonde.mp3" type="audio/mpeg" />
-            <source src="/nikukonde.mp3" type="audio/mpeg" />
+            <source src="/away-park-feat-kaiit.mp3" type="audio/mpeg" />
+            <source src="/away-park-feat-kaiit.mp3" type="audio/mpeg" />
       </audio>
       </div>
   
